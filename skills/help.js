80202975857ef13ad7b0e293c4ed3b9b7eff4a93
paolaMancini <paola.mancini@italtel.com>
2018-07-13 17:21:46 +0200

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -3,7 +3,7 @@
 //
 module.exports = function (dialogflowMiddleware,controller) {
 
-    controller.hears(['getSkills',/^help$/], 'direct_message,direct_mention',dialogflowMiddleware.hears function (bot, message) {
+    controller.hears(['getSkills',/^help$/], 'direct_message,direct_mention',dialogflowMiddleware.hears, function (bot, message) {
         var text = "Here are my skills:";
         text += "\n- " + bot.appendMention(message, "open <Digitaliani|Office 301>") + ": let a user to receive the url granting access to the smart lock;"
         text += "\n- " + bot.appendMention(message, "locks") + ": let a user to receive datails about smart locks";
