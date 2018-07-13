37a7da986fb55626018d6362752cdc54bfda16a0
paolaMancini <paola.mancini@italtel.com>
2018-07-13 16:29:57 +0200

    Update a-hello.js

--- skills/a-hello.js
+++ skills/a-hello.js
@@ -1,13 +1,11 @@
 module.exports = function( dialogflowMiddleware,controller) {
 
     controller.hears('Default Welcome Intent', "direct_message,direct_mention",dialogflowMiddleware.hears,  function(
-    //controller.hears(['aboutMe', /^sorry.*/i], "direct_message,direct_mention",   function(
-    //controller.hears(['aboutMe', /^sorry.*/i], "direct_message,direct_mention", dialogflowMiddleware.hears, function(
         bot,
         message
     ) {
         console.log('MESSAGE:', message);
-        console.log('fullfilment:', message.fulfillmentText);
+        console.log('fulfillment:', message.fulfillment);
         bot.reply(message, 'Ciao!');
     });
 }
