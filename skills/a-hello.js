1f9f6d894c4f0a3408587f5725266a52ea933dee
paolaMancini <paola.mancini@italtel.com>
2018-07-13 15:40:20 +0200

    Update a-hello.js

--- skills/a-hello.js
+++ skills/a-hello.js
@@ -1,6 +1,6 @@
 module.exports = function(controller) {
 
-    controller.hears('aboutMe', 'direct_message', dialogflowMiddleware.hears, function(
+    controller.hears('aboutMe', "direct_message,direct_mention", dialogflowMiddleware.hears, function(
         bot,
         message
     ) {
