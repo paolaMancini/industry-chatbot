8b66912c3aecd5632757c89b5bd55f135d7d2379
paolaMancini <paola.mancini@italtel.com>
2018-07-16 16:39:01 +0200

    Update whoIam.js

--- skills/whoIam.js
+++ skills/whoIam.js
@@ -1,6 +1,6 @@
 module.exports = function( dialogflowMiddleware,controller) { {
 
-    controller.hears('smalltalk.agent.acquaintance', [/who are you|about you|who are u|about u/i], 'direct_message,direct_mention',dialogflowMiddleware.hears, function(bot, message) {
+    controller.hears(['smalltalk.agent.acquaintance', /who are you|about you|who are u|about u/i], 'direct_message,direct_mention',dialogflowMiddleware.hears, function(bot, message) {
         console.log('emails: ',message.emails);
         var mardown = "This is Clavigero, the Keys' Keeper. I can help you to obtain the link to use by your phone (or your tablet) in order to open your JAGO door lock.";
          
