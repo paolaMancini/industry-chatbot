99e9a7ef2bf5095423080d7d330083622e60bace
paolaMancini <paola.mancini@italtel.com>
2019-02-14 10:41:02 +0100

    New commit equals to the previuous one

--- skills/whoIam.js
+++ skills/whoIam.js
@@ -2,8 +2,8 @@ module.exports = function( dialogflowMiddleware,controller) { {
 
     controller.hears(['smalltalk.agent.acquaintance', /who are you|about you|who are u|about u/i], 'direct_message,direct_mention',dialogflowMiddleware.hears, function(bot, message) {
         console.log('emails: ',message.emails);
-        var mardown = "This is Clavigero (v3), the Keys' Keeper. I can help you to obtain the link to use by your phone (or your tablet) in order to open your JAGO door lock.";
+        var markdown = "This is Clavigero (v3), the Keys' Keeper. I can help you to obtain the link to use by your phone (or your tablet) in order to open your JAGO door lock.";
          
-        bot.reply(message, mardown);
+        bot.reply(message, markdown);
     });
 }
