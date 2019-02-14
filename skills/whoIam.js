79abc591a942a57aa3caef310361d9cf481aae32
paolaMancini <paola.mancini@italtel.com>
2019-02-14 10:37:29 +0100

    Added version number in the text message

--- skills/whoIam.js
+++ skills/whoIam.js
@@ -2,7 +2,7 @@ module.exports = function( dialogflowMiddleware,controller) { {
 
     controller.hears(['smalltalk.agent.acquaintance', /who are you|about you|who are u|about u/i], 'direct_message,direct_mention',dialogflowMiddleware.hears, function(bot, message) {
         console.log('emails: ',message.emails);
-        var mardown = "This is Clavigero, the Keys' Keeper. I can help you to obtain the link to use by your phone (or your tablet) in order to open your JAGO door lock.";
+        var mardown = "This is Clavigero (v3), the Keys' Keeper. I can help you to obtain the link to use by your phone (or your tablet) in order to open your JAGO door lock.";
          
         bot.reply(message, mardown);
     });
