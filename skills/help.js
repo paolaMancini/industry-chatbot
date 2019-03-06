ecc1b149878b3f543cf8f3733dbdffa506e88619
paolaMancini <paola.mancini@italtel.com>
2019-03-06 16:53:34 +0100

    Changed locks with rooms

--- skills/help.js
+++ skills/help.js
@@ -5,8 +5,8 @@ module.exports = function (dialogflowMiddleware,controller) {
 
     controller.hears(['getSkills',/^help$/], 'direct_message,direct_mention',dialogflowMiddleware.hears, function (bot, message) {
         var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "open <Digitaliani|Office 301>") + ": let a user to receive the url granting access to the smart lock;"
-        text += "\n- " + bot.appendMention(message, "locks") + ": let a user to receive datails about smart locks";
+        text += "\n- " + bot.appendMention(message, "open <Digitaliani|Office 301>") + ": let a user to receive the url granting the access to the room (or smart lock);"
+        text += "\n- " + bot.appendMention(message, "locks") + ": let a user to receive details about the rooms (or smart locks) it knows";
          
         bot.reply(message, text);
     });
